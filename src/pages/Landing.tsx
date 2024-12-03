1295771d143a39f4edfa677c7d7ade0545761dab
irvine kwambana <irvenehawks@gmail.com>
2024-12-03 16:57:07 +0200

    second commit

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 import About from '../components/About'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
